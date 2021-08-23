234191ad3281e1fd263c38644b3d06164eabb975
thom8047 <55425085+thom8047@users.noreply.github.com>
2021-08-23 12:11:57 -0600

    Add files via upload

---
+++ JavaTest/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String []args) {
+        System.out.println("Hello World"); // prints Hello World
+
+        imported_test OBJ = new imported_test();
+        System.out.println(OBJ.openNewObj());
+     }
+}
---
+++ JavaTest/imported_test.java
@@ -0,0 +1,5 @@
+public class imported_test {
+    public String openNewObj() {
+        return "this object";
+    }
+}
